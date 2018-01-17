aa3e81489193193c60ae85aeba8ac006ab366833
MicCabeza22 <cabeza.michael@yahoo.com>
2018-01-17 16:00:41 -0600

    Refactored and added files to set up a view.

--- src/main/java/com/codeup/springbootblog/HomeController.java
+++ src/main/java/com/codeup/springbootblog/HomeController.java
@@ -2,13 +2,11 @@ package com.codeup.springbootblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/home")
     public String landingPage() {
-        return "This is the landing page!";
+        return "home";
     }
 }
